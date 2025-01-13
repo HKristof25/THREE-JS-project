33b0a87fa0400fdaae7a99f161652fa7ee3a0087
Hajdú Kristóf <21.hajdu.kristof@mechwart.com>
2025-01-13 19:45:34 +0100

    Menu

--- Web Page/src/menu.js
+++ Web Page/src/menu.js
@@ -9,7 +9,6 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { cubeMapNode } from 'three/src/nodes/utils/CubeMapNode.js';
 import { PointsNodeMaterial } from 'three/webgpu';
 
-//Three js setup dont touch it
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1,1000);
@@ -20,8 +19,10 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight)
-camera.position.set(1,1,1)
-//camera.position.set( -5.47283435693948,8.719480629687583,12.0075850771697);
+camera.position.set( 9,9,9);
+camera.rotateX(-0.78)
+camera.rotateY(0.61)
+camera.rotateZ(0.52)
 
 renderer.render(scene,camera)
 
@@ -69,18 +70,10 @@ light.shadow.mapSize.height = 512
 light.shadow.camera.near= 0.5
 light.shadow.camera.far= 500
 
-//Shows shadow and lighting directions
-const helper = new THREE.CameraHelper( light.shadow.camera );
-//scene.add( helper );
-
 const ambientLight = new THREE.AmbientLight(0xffffff, 2)
 
 scene.add(ambientLight)
 
-//Camera Control
-const controls = new OrbitControls(camera,renderer.domElement)
-
-//Adding background stars
 function addStar(){
   const geometry = new THREE.SphereGeometry(0.25,24,24)
   const material = new THREE.MeshStandardMaterial({color: 0xffffff})
@@ -93,64 +86,13 @@ function addStar(){
 
 Array(200).fill().forEach(addStar)
 
-//On Mouse Hover
-
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
-
-document.addEventListener('mousemove', onPointerMove)
-let INTERSECTED;
-
-function onPointerMove( event ) {
-
-	// calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-}
-
-function render() {
-
-	// update the picking ray with the camera and pointer position
-	raycaster.setFromCamera( pointer, camera );
-
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( scene.children, false );
-
-  if (intersects.length > 0 ){
-    if (INTERSECTED != intersects[0].object){
-      if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-						INTERSECTED = intersects[ 0 ].object;
-            if(INTERSECTED.name == "Cube")
-            {
-              INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-              INTERSECTED.material.emissive.setHex( 0x0080c9 );
-              INTERSECTED.position.y += 1
-            }
-    }
-  }else {
-
-    if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-    INTERSECTED = null;
-
-  }
-
-
-
-}
-
-
-
-window.requestAnimationFrame(render);
 //Function called every frame
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.autoRotate = true
 function animate(){
-  console.log(camera.position, camera.rotation)
   requestAnimationFrame(animate)
-  render()
+  controls.enabled = false
+  controls.update()
   renderer.render(scene,camera)
 }
 animate()
